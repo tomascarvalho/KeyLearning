c20ee3f109ed69e91d027ebbddbdb1a3603aa4f2
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2016-12-12 19:33:28 +0100

    still not working?

--- KeyLearning/settings.py
+++ KeyLearning/settings.py
@@ -130,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:14243160@localhost:5432/postgres')}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:14243160@localhost:5432/postgres')}
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
