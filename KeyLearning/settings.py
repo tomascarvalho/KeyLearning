66736fc69502e3b4bcf253524968ab83e6f19ca5
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2016-12-12 14:44:47 +0100

    mmm

--- KeyLearning/settings.py
+++ KeyLearning/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+DATABASES['default'] =  dj_database_url.config()
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
